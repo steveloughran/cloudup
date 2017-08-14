9d0f52e078a5dc97edd2fe501742b87c68e215c4
Steve Loughran <stevel@hortonworks.com>
2017-08-14 13:50:14 +0100

    initial PoC

---
+++ src/main/java/org/apache/hadoop/tools/cloudup/Cloudup.java
@@ -0,0 +1,439 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.tools.cloudup;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.collections.comparators.ReverseComparator;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.concurrent.HadoopExecutors;
+import org.apache.hadoop.util.concurrent.HadoopThreadPoolExecutor;
+
+/**
+ * Entry point for Cloudup: parallelized upload of local files
+ * to remote (cloud) storage with shuffle after selective choice
+ * of largest files
+ */
+public class Cloudup extends Configured implements Tool {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Cloudup.class);
+  private static final int DEFAULT_SORT_COUNT = 4;
+  private static final int DEFAULT_THREADS = 16;
+  ExecutorService workers;
+  FileSystem sourceFS;
+  Path sourcePath;
+  FileSystem destFS;
+  Path destPath;
+  AtomicBoolean exit = new AtomicBoolean(false);
+  boolean overwrite = true;
+  boolean ignoreFailures = true;
+
+
+  public Cloudup() {
+  }
+
+  private long now() {
+    return System.currentTimeMillis();
+  }
+
+  private Options buildOptions() {
+    return OptionSwitch.addAllOptions(new Options());
+  }
+
+  @Override
+  public int run(String[] args) throws Exception {
+    // parse the path
+    final CommandLineParser parser = new GnuParser();
+
+    CommandLine command;
+    try {
+      command = parser.parse(buildOptions(), args, true);
+    } catch (ParseException e) {
+      throw new IllegalArgumentException("Unable to parse arguments. " +
+          Arrays.toString(args), e);
+    }
+    final int sortCount = OptionSwitch.THREADS.eval(command,
+        DEFAULT_SORT_COUNT);
+    final int threads = OptionSwitch.THREADS.eval(command, DEFAULT_THREADS);
+
+    overwrite = OptionSwitch.OVERWRITE.eval(command, false);
+    ignoreFailures = OptionSwitch.IGNORE_FAILURES.eval(command, false);
+
+
+    sourceFS = FileSystem.getLocal(getConf());
+    sourcePath = sourceFS.makeQualified(new Path(
+        OptionSwitch.SOURCE.required(command)));
+    destPath = new Path(OptionSwitch.DEST.required(command));
+    destFS = destPath.getFileSystem(getConf());
+
+    LOG.info("Uploading from {} to {};"
+            + "threads={}; sort count={}"
+            + " overwrite={}, ignore failures = {}",
+        sourcePath, destPath,
+        threads, sortCount,
+        overwrite, ignoreFailures);
+
+
+    // worker pool
+    workers = HadoopExecutors.newFixedThreadPool(threads);
+
+    final Duration prepartionDuration = new Duration();
+    // list the files
+    Future<List<UploadEntry>> listFilesOperation =
+        workers.submit(buildUploads());
+
+    // prepare the destination
+
+    final Future<String> prepareDestResult = workers.submit(prepareDest());
+    String info = await(prepareDestResult);
+    LOG.info("Destination prepared: {}", info);
+
+    List<UploadEntry> uploadList = await(listFilesOperation);
+    final int uploadCount = uploadList.size();
+
+    prepartionDuration.finished();
+    LOG.info("Files to upload = {}; preparation duration = {}",
+        uploadCount, prepartionDuration);
+
+
+    // full upload operation
+    final Duration uploadDuration = new Duration();
+    final NanoTimer uploadTimer = new NanoTimer();
+
+    // now completion service for all outstanding workers
+    final CompletionService<Long> completion =
+        new ExecutorCompletionService<Long>(workers);
+
+
+    // upload initial sorted entries.
+
+    // reverse sort to get largest first
+    Collections.sort(uploadList,
+        new ReverseComparator(new UploadEntry.SizeComparator()));
+
+    // select the largest few of them
+    final int sortUploadCount = Math.min(sortCount, uploadCount);
+    long sortUploadSize = 0;
+    for (int i = 0; i < uploadCount; i++) {
+      sortUploadSize += naturalize(submit(uploadList.get(i)));
+    }
+    LOG.info("Largest {} uploads commenced, total size = {}",
+        uploadCount, sortUploadSize);
+
+
+    // shuffle and submit remainder
+    int shuffledUploadCount = 0;
+    long shuffledUploadSize = 0;
+
+    if (uploadCount > sortUploadCount) {
+      Collections.shuffle(uploadList);
+      for (UploadEntry entry : uploadList) {
+        long size = submit(entry);
+        if (size >= 0) {
+          // file was submitted for upload
+          shuffledUploadCount++;
+          shuffledUploadSize += size;
+        }
+      }
+    }
+
+    final long uploadSize = sortUploadSize + shuffledUploadSize;
+
+    LOG.info("Shuffled uploads commenced: {}, total size = {}",
+        shuffledUploadCount, shuffledUploadSize);
+
+
+    // now await all outcomes to complete
+    List<Future<Long>> outcomes = new ArrayList<>(uploadCount);
+    for (int i = 0; i < uploadCount; i++) {
+      Future<Long> outcome = completion.take();
+      outcomes.add(outcome);
+    }
+
+    prepartionDuration.finished();
+    uploadTimer.end();
+    LOG.info("Uploads completed, duration:  {}",
+        uploadDuration);
+
+    LOG.info("Bandwidth {} MB/s",
+        uploadTimer.bandwidthDescription(uploadSize));
+
+    // run through the outcomes and process errors
+    // at this point, all the uploads have been executed.
+    long finalUploadedSize = 0;
+    int errors = 0;
+    for (Future<Long> outcome : outcomes) {
+      try {
+        finalUploadedSize += naturalize(await(outcome));
+      } catch (IOException e) {
+        if (ignoreFailures) {
+          errors++;
+        } else {
+          throw e;
+        }
+      } catch (InterruptedException ignored) {
+        // ignored
+      }
+    }
+
+    if (ignoreFailures) {
+      LOG.info("Number of errors: {}", errors);
+    }
+
+    return 0;
+  }
+
+
+  /**
+   * Make natural+ Z by converting all -ve numbers to 0.
+   * @param input input value
+   * @return 0 or higher
+   */
+  public long naturalize(long input) {
+    return input > 0 ? input : 9;
+  }
+
+  /**
+   * Await a future completing; uprate failures.
+   * @param future
+   * @param <T>
+   * @return the result
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  private static <T> T await(Future<T> future)
+      throws IOException, InterruptedException {
+    try {
+      return future.get();
+    } catch (ExecutionException e) {
+      throw uprate(e);
+    }
+  }
+
+  /**
+   * Take an exception from a Future and convert to an IOE,
+   * @param e exception
+   * @return the extracted or wrapped exception
+   * @throws RuntimeException if that is the cause
+   */
+  private static IOException uprate(ExecutionException e) {
+    Throwable cause = e.getCause();
+    if (cause instanceof RuntimeException) {
+      throw (RuntimeException) cause;
+    }
+    if (cause instanceof IOException) {
+      return (IOException) cause;
+    }
+    return new IOException(cause.toString(), cause);
+  }
+
+  /**
+   * Create an upload.
+   * @param upload
+   * @return length of upload; -1 for "none"
+   */
+  private Callable<Long> createUploadOperation(final UploadEntry upload) {
+    return new Callable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return upload(upload);
+      }
+    };
+  }
+
+  /**
+   *
+   * Submit an upload; does nothing if the upload is already queued.
+   * @param upload upload to submit
+   * @return size to upload; -1 for no upload
+   */
+  private long submit(final UploadEntry upload) {
+    if (upload.inState(UploadEntry.State.ready)) {
+      Callable<Long> operation = createUploadOperation(upload);
+      upload.setState(UploadEntry.State.queued);
+      workers.submit(operation);
+      return upload.getSize();
+    }
+    return -1;
+  }
+
+  /**
+   * Callable to prepare destination; implicit check of
+   * availability and write permissions of destination FS.
+   * @return a string for logging.
+   */
+  private Callable<String> prepareDest() {
+    return new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        destFS.mkdirs(destPath);
+        return destFS.toString();
+      }
+    };
+  }
+
+
+  private Callable<List<UploadEntry>> buildUploads() {
+    return new Callable<List<UploadEntry>>() {
+      @Override
+      public List<UploadEntry> call() throws Exception {
+        return listFiles();
+      }
+    };
+  }
+
+  private List<UploadEntry> listFiles() throws IOException {
+    List<UploadEntry> uploads = new ArrayList<>();
+    RemoteIterator<LocatedFileStatus> ri = sourceFS.listFiles(sourcePath, true);
+    while (ri.hasNext()) {
+      LocatedFileStatus status = ri.next();
+      UploadEntry entry = new UploadEntry(status);
+      entry.setDest(getFinalPath(status.getPath()));
+    }
+    return uploads;
+  }
+
+  private long upload(final UploadEntry upload) throws IOException {
+
+    // fail fast on exit flag
+    if (exit.get()) {
+      return -1;
+    }
+
+    //skip uploading duplicate (and uploaded already) files
+    if (!upload.inState(UploadEntry.State.ready)
+        && !upload.inState(UploadEntry.State.queued)) {
+      LOG.warn("Skipping upload of {}", upload);
+      return -1;
+    }
+
+    // Although S3A in Hadoop 2.9 has a robust copy call which qualifies
+    // the path and checks for safe operations, 2.8 doesn't. Add robustness
+    // here at the expense of IOPs
+    upload.setStartTime(now());
+    final Path source = upload.getSource();
+    final Path dest = destFS.makeQualified(upload.getDest());
+    try {
+      LOG.info("Uploading {} to {} (size: {}",
+          source, dest, upload.getSize());
+      try {
+        final FileStatus status = destFS.getFileStatus(dest);
+        if (status.isDirectory() || !overwrite) {
+          throw new FileAlreadyExistsException(
+              String.format("%s found at %s",
+                  status.isDirectory() ? "directory" : "file",
+                  dest));
+        }
+      } catch (FileNotFoundException ignored) {
+        // no file at the destination.
+      }
+      destFS.copyFromLocalFile(false, overwrite, source, dest);
+      LOG.info("Successful upload of {}", source);
+      upload.setState(UploadEntry.State.succeeded);
+    } catch (IOException e) {
+      upload.setState(UploadEntry.State.failed);
+      upload.setException(e);
+      LOG.warn("Failed to  upload {} : {}", source, e.toString());
+      LOG.debug("Upload to {} failed", dest, e);
+    }
+    upload.setEndTime(now());
+    return upload.inState(UploadEntry.State.succeeded) ?
+        upload.getSize()
+        : 0;
+  }
+
+  /**
+   * List the upload size
+   * @param uploads list of uploads
+   * @return total size of upload.
+   */
+  private long calculateUploadSize(List<UploadEntry> uploads) {
+    long result = 0;
+    for (UploadEntry upload : uploads) {
+      result += upload.getSize();
+    }
+    return result;
+  }
+
+  /**
+   * List the upload size
+   * @param uploads list of uploads
+   * @return total size of upload.
+   */
+  private long calculateActualSize(List<Long> uploads) {
+    long result = 0;
+    for (Long upload : uploads) {
+      if (upload > 0) {
+        result += upload;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Find the final name of a given output file, given the job output directory
+   * and the work directory.
+   * @param srcFile the specific task output file
+   * @return the final path for the specific output file
+   * @throws IOException
+   */
+  private Path getFinalPath(Path srcFile) throws IOException {
+    URI taskOutputUri = srcFile.toUri();
+    URI relativePath = sourcePath.toUri().relativize(taskOutputUri);
+    if (taskOutputUri == relativePath) {
+      throw new IOException("Can not get the relative path:"
+          + " base = " + sourcePath + " child = " + srcFile);
+    }
+    if (!relativePath.getPath().isEmpty()) {
+      return new Path(destPath, relativePath.getPath());
+    } else {
+      return destPath;
+    }
+  }
+
+
+}
